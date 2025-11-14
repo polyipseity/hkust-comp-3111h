3adafb12b9dfde3cd8b838ce41a59fb738f930e8
HUI, Chun Wai <cwhuiaf@connect.ust.hk>
2025-11-14 14:57:57 +0800

    Update test case for Repository

--- src/test/java/library/persistence/RepositoryTest.java
+++ src/test/java/library/persistence/RepositoryTest.java
@@ -70,9 +70,9 @@ class RepositoryTest {
 	@Test
 	void bookCreateReadUpdateDelete() {
 		var book = new Book("Clean Code", new Author.ByName("Robert C. Martin"));
-		var data = new Book.Data("Good book!", Book.ApprovalStatus.APPROVED, null, Collections.singletonMap(new User("alice"), new Book.Borrow(new Date(), Duration.ofMillis(42), new ByteArray(new byte[42]))), 5);
+		var data = new Book.Data("Good book!", "Good book content", Book.ApprovalStatus.APPROVED, null, Collections.singletonMap(new User("alice"), new Book.Borrow(new Date(), Duration.ofMillis(42), new ByteArray(new byte[42]))), 5);
 		var book2 = new Book("Dirty Code", new Author.ByRef(new User("alice")), true);
-		var data2 = new Book.Data("Bad book!", Book.ApprovalStatus.PENDING, new Book("Dirty Code", new Author.ByRef(new User("alice"))), Collections.emptyMap(), 0);
+		var data2 = new Book.Data("Bad book!", "Bad book content", Book.ApprovalStatus.PENDING, new Book("Dirty Code", new Author.ByRef(new User("alice"))), Collections.emptyMap(), 0);
 
 		// ---- create ---------------------------------------------------------
 		assertDoesNotThrow(() -> service.createBook(book, data), "createBook should not throw when the key is new");
