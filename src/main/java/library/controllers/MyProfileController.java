e2d164dd127d517735faf0b59962e7f873b5e506
HUI, Chun Wai <cwhuiaf@connect.ust.hk>
2025-11-16 23:58:00 +0800

    Implement "My Profile" tab logic

--- src/main/java/library/controllers/MyProfileController.java
+++ src/main/java/library/controllers/MyProfileController.java
@@ -1,21 +1,61 @@
 package library.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import library.Context;
 import library.Main;
+import library.controls.UserValidator;
 import library.models.User;
+import library.persistence.Repository;
+import library.persistence.TransactionException;
+import library.utils.Alerts;
+
+import java.util.Objects;
 
 public class MyProfileController {
+	private final Context context = Main.getContext();
+	private final Repository repository = context.getRepository();
+	private final User user = Main.getContext().getLoggedInUser()._1();
+	private final User.Data userData = Main.getContext().getLoggedInUser()._2();
+
 	@FXML
 	public Text usernameText;
-	private User currentUser;
-	private User.Data currentUserData;
+	@FXML
+	public TextField newNameField, newPasswordField;
 
 	@FXML
 	private void initialize() {
-		assert Main.getContext().getLoggedInUser() != null;
-		currentUser = Main.getContext().getLoggedInUser()._1();
-		currentUserData = Main.getContext().getLoggedInUser()._2();
-		usernameText.setText(currentUser.username());
+		usernameText.setText(user.username());
+	}
+
+	@FXML
+	private void updateProfile() {
+		String enteredPassword = newPasswordField.getText();
+		String enteredFullName = newNameField.getText();
+		if (enteredPassword.isEmpty() && enteredFullName.isEmpty()) {
+			Alerts.showErrorDialog("Both input fields are empty");
+			return;
+		}
+		String newPassword = enteredPassword.isEmpty() ? userData.password() : enteredPassword;
+		String newFullName = enteredFullName.isEmpty() ? userData.fullName() : enteredFullName;
+		UserValidator.Result result = UserValidator.DEFAULT.apply(user.username(), newPassword, newFullName);
+		switch (result) {
+			case UserValidator.Result.Success() -> {
+				User.Data newUserData = userData
+						.withPassword(newPassword)
+						.withFullName(newFullName);
+				try {
+					repository.userOps.update(user, (_) -> newUserData);
+					Alerts.showInfoDialog("Updated successfully");
+				} catch (TransactionException e) {
+					Alerts.showErrorDialog("Update Failed: " + e.getMessage());
+				}
+			}
+			case UserValidator.Result.BadFullName(String message) -> Alerts.showErrorDialog(message);
+			case UserValidator.Result.BadPassword(String message) -> Alerts.showErrorDialog(message);
+			default -> throw new IllegalStateException("Unexpected value: " + result);
+		}
 	}
 }
