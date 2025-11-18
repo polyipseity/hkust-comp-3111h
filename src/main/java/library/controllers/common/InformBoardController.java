ce4dd9026ff901f43640a8bc6a243f1854b82644
HUI, Chun Wai <cwhuiaf@connect.ust.hk>
2025-11-19 00:36:10 +0800

    Update InformBoardController.java

--- src/main/java/library/controllers/common/InformBoardController.java
+++ src/main/java/library/controllers/common/InformBoardController.java
@@ -3,32 +3,71 @@ package library.controllers.common;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import library.Main;
 import library.persistence.Repository;
 import library.persistence.TransactionException;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class InformBoardController implements RequiresLoggedIn {
     Repository repository = Main.getContext().getRepository();
 
-    //Refering to fxml ListView
+    //Referring to fxml ListView
     @FXML
     private ListView<String> NotificationList;
 
     //Storing all notifications of user
     private final ObservableList<String> notifications = FXCollections.observableArrayList();
 
+    class NotificationCell extends ListCell<String> {
+        HBox hbox = new HBox();
+        Label label = new Label();
+        Pane pane = new Pane();
+        Button button = new Button("Close");
+        String lastItem;
+
+        public NotificationCell() {
+            super();
+            hbox.getChildren().addAll(label, pane, button);
+            HBox.setHgrow(pane, Priority.ALWAYS);
+            button.setOnAction(_ -> {
+                try {
+                    CloseNotif(label.getText());
+                } catch (TransactionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            setText(null);
+            if (empty) {
+                lastItem = null;
+                setGraphic(null);
+            } else {
+                lastItem = item;
+                label.setText(item != null ? item : "<null>");
+                setGraphic(hbox);
+            }
+        }
+    }
+
 	@Override
 	public void initialize(@Nullable URL location, @Nullable ResourceBundle resources) {
 		RequiresLoggedIn.super.initialize(location, resources);
 
+        NotificationList.setCellFactory(_ -> new NotificationCell());
 		NotificationList.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/Notification.css")).toExternalForm());
 		NotificationList.setFocusTraversable(false);
 		updateNotificationList();
@@ -52,6 +91,17 @@ public class InformBoardController implements RequiresLoggedIn {
             String[] updated = Arrays.copyOf(currentNotifications, currentNotifications.length + 1);
             updated[updated.length - 1] = "New notification message " + updated.length;
             return updated;});
+        updateNotificationList();
+    }
+
+    private void CloseNotif(String notificationText) throws TransactionException {
+        repository.userNotificationOps.update(
+                getLoggedInUser()._1(),
+                current -> {
+                    List<String> newNotifList = new ArrayList<>(Arrays.asList(current));
+                    newNotifList.remove(notificationText);
+                    return newNotifList.toArray(new String[0]);
+                });
         updateNotificationList();
     }
 }
