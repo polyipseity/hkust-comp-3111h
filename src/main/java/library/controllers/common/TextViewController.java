57d85713b6fa4470a1855e7532ac7fca7350b9e7
William So <polyipseity@gmail.com>
2025-11-20 10:10:28 +0800

    Fix `TextViewController`

--- src/main/java/library/controllers/common/TextViewController.java
+++ src/main/java/library/controllers/common/TextViewController.java
@@ -1,7 +1,6 @@
 package library.controllers.common;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Font;
@@ -9,11 +8,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public final class TextViewController implements RequiresLoggedIn, Initializable {
-	public static final double CONTENT_SCROLL_BAR_PREF_WIDTH = 20;
-
 	public TextArea contentArea;
 	public Slider zoomSlider;
 
@@ -21,8 +19,7 @@ public final class TextViewController implements RequiresLoggedIn, Initializable
 	public void initialize(@Nullable URL location, @Nullable ResourceBundle resources) {
 		RequiresLoggedIn.super.initialize(location, resources);
 
-		ScrollBar vertScrollBar = (ScrollBar) contentArea.lookup(".scroll-bar:vertical");
-		vertScrollBar.setPrefWidth(CONTENT_SCROLL_BAR_PREF_WIDTH);
+		contentArea.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/common/TextViewContentArea.css")).toExternalForm());
 		zoomSlider.valueProperty().addListener((_, _, newValue) ->
 				contentArea.setFont(Font.font(contentArea.getFont().getFamily(), newValue.doubleValue())));
 	}
