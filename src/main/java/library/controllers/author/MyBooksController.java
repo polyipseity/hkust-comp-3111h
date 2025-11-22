dfc24f177c4a3f5769cdfee58b4922dbfaea209e
William So <polyipseity@gmail.com>
2025-11-22 11:49:14 +0800

    Refactor `MyBooksController`

--- src/main/java/library/controllers/author/MyBooksController.java
+++ src/main/java/library/controllers/author/MyBooksController.java
@@ -1,6 +1,7 @@
 package library.controllers.author;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -12,6 +13,8 @@ import library.controllers.common.TextViewController;
 import library.controls.ManageBooksControl;
 import library.models.Author;
 import library.models.Book;
+import library.models.Borrow;
+import library.models.User;
 import library.persistence.TransactionException;
 import library.utils.Alerts;
 import library.utils.HasMessage;
@@ -23,6 +26,7 @@ import org.jetbrains.annotations.UnknownNullability;
 import java.io.IOException;
 import java.net.URL;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.function.Function;
@@ -32,8 +36,11 @@ public final class MyBooksController implements RequiresLoggedIn, Initializable 
 	public TableView<@Nullable Data> table;
 	@UnknownNullability
 	public DynamicTableController<Keys, Data> tableController;
+
 	@UnknownNullability
 	public TableColumn<Data, @Nullable Data> titleCol, statusCol, dateCol, readersCol, summaryCol;
+	@UnknownNullability
+	public Button viewButton, modifyButton, deleteButton;
 
 	@Override
 	public void initialize(@Nullable URL location, @Nullable ResourceBundle resources) {
@@ -47,41 +54,48 @@ public final class MyBooksController implements RequiresLoggedIn, Initializable 
 		keys.put(Keys.SUMMARY, summaryCol);
 		tableController = new DynamicTableController<>(table, keys);
 
+		final var selectedItem = table.getSelectionModel().selectedItemProperty();
+		viewButton.disableProperty().bind(selectedItem.isNull());
+		modifyButton.disableProperty().bind(selectedItem.map(item -> item == null || !item.borrows.isEmpty()).orElse(true));
+		deleteButton.disableProperty().bind(selectedItem.map(item -> item == null || !item.borrows.isEmpty()).orElse(true));
+
 		loadTable();
 	}
 
 	public void loadTable() {
 		final var author = new Author.ByRef(getLoggedInUser()._1());
-		tableController.setData(Main.getContext().getRepository().bookOps
+		final var repository = Main.getContext().getRepository();
+		tableController.setData(repository.bookOps
 				.read(entry -> entry.getValue().active() && author.equals(entry.getKey().author()))
-				.entrySet().stream().map(entry -> new MyBooksController.Data(this, entry.getKey(), entry.getValue()))
+				.entrySet().stream().map(entry ->
+						new MyBooksController.Data(
+								this,
+								entry.getKey(),
+								entry.getValue(),
+								repository.borrowOps.read(entry.getKey())
+						))
 				.toList());
 	}
 
-	public void viewBook() {
+	public void viewBook() throws IOException {
 		final var selected = table.getSelectionModel().getSelectedItem();
 		if (selected == null) {
-			Alerts.showErrorDialog("Please select a book first.");
+			// Button should be disabled
 			return;
 		}
-		try {
 			Main.getContext().newWindow(
 					TextViewController.WINDOW_TITLE.formatted(selected.book.title()),
 					FXMLs.COMMON_TEXT_VIEW.load(loader -> loader.<TextViewController>getController().setContent(selected.bookData.content())),
 					null
 			).show();
-		} catch (IOException e) {
-			Alerts.showErrorDialog(e.getLocalizedMessage());
-		}
 	}
 
 	public void modifyBook() throws IOException {
 		final var selected = table.getSelectionModel().getSelectedItem();
-		if (selected == null) {
-			Alerts.showErrorDialog("Please select a book first.");
+		if (selected == null || !selected.borrows.isEmpty()) {
+			// Button should be disabled
 			return;
 		}
-		// Create a new Stage (window) and show it
 		Main.getContext().newWindow(
 				"Modify Book",
 				// Load the FXML file for the new window's content
@@ -98,14 +112,14 @@ public final class MyBooksController implements RequiresLoggedIn, Initializable 
 
 	public void deleteBook() {
 		final var selected = table.getSelectionModel().getSelectedItem();
-		if (selected == null) {
-			Alerts.showErrorDialog("Please select a book first.");
-			return;
-		}
-		if (!deleteConfirmation(selected.book.title())) {
+		if (selected == null || !selected.borrows.isEmpty()) {
+			// Button should be disabled
 			return;
 		}
 
+		if (!deleteConfirmation(selected.book.title())) {
+			return;
+		}
 		try {
 			switch (Main.getContext().getManageBooksControl().deleteBook(selected.book, getLoggedInUser()._2().role())) {
 				//Reload table after deleting a book
@@ -133,7 +147,8 @@ public final class MyBooksController implements RequiresLoggedIn, Initializable 
 
 	public record Data(MyBooksController controller,
 	                   Book book,
-	                   Book.Data bookData)
+	                   Book.Data bookData,
+	                   Map<User, Borrow> borrows)
 			implements Function<Keys, DynamicTableController.Data> {
 		/**
 		 * Applies this function to the given argument.
@@ -143,7 +158,6 @@ public final class MyBooksController implements RequiresLoggedIn, Initializable 
 		 */
 		@Override
 		public DynamicTableController.Data apply(Keys key) {
-			var borrowOfBook = Main.getContext().getRepository().borrowOps.read(book);
 			return switch (key) {
 				case TITLE -> new DynamicTableController.Data.Text(book.title());
 				case STATUS -> new DynamicTableController.Data.Text(bookData.approvalStatus().name);
@@ -151,8 +165,7 @@ public final class MyBooksController implements RequiresLoggedIn, Initializable 
 						bookData.publishDate() == null
 								? ""
 								: TimeUtil.toStringZonedLocal(bookData.publishDate()));
-				case READERS ->
-						new DynamicTableController.Data.Text(String.valueOf(borrowOfBook.isEmpty() ? 0 : borrowOfBook.size()));
+				case READERS -> new DynamicTableController.Data.Text(String.valueOf(borrows.size()));
 				case SUMMARY -> new DynamicTableController.Data.Text(bookData.summary());
 			};
 		}
